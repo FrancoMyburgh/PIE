c06a338fe08eba4a41a1306c820a995b3ed001e7
Franco Myburgh <franco@brainygiraffe.com>
2025-11-03 23:12:40 +0200

    Fix to Gradle build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,11 +21,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "https://pieparking.co.za"
+        applicationId = "co.za.pieparking.pie"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         //minSdk = flutter.minSdkVersion
-        minSdk = localProperties.getProperty('flutter.minSdkVersion').toInteger()
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
